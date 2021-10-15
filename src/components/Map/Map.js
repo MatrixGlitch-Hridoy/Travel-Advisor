923a213c3c1eb4f0ac2545f5c97fa56d517c478c
Hridoy <rkhridoy68@gmail.com>
2021-10-15 16:33:05 +0600

    restaurant details showed in the map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
-import { useMediaQuery } from '@material-ui/core';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
 import useStyles from './style';
 const Map = ({setCoordinates, setBounds, coordinates, places}) => {
     const classes = useStyles();
-    const isMobile = useMediaQuery('(min-width:600px)');
+    const isDesktop = useMediaQuery('(min-width:600px)');
 
     return (
         <div className={classes.mapContainer}>
@@ -21,6 +23,28 @@ const Map = ({setCoordinates, setBounds, coordinates, places}) => {
                 }}
                 onChildClick={''}
             >
+                {places?.map((place, i) => (
+          <div
+            className={classes.markerContainer}
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key={i}
+          >
+            {!isDesktop
+              ? <LocationOnOutlinedIcon color="primary" fontSize="large" />
+              : (
+                <Paper elevation={3} className={classes.paper}>
+                  <Typography className={classes.typography} variant="subtitle2" gutterBottom> {place.name}</Typography>
+                  <img
+                    className={classes.pointer}
+                    src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                    alt={place.name}
+                  />
+                  <Rating name="read-only" size="small" value={Number(place.rating)} readOnly />
+                </Paper>
+              )}
+          </div>
+        ))}
 
             </GoogleMapReact>
         </div>
--- src/components/PlaceDetails/PlaceDetails.js
+++ src/components/PlaceDetails/PlaceDetails.js
@@ -10,6 +10,7 @@ import {
 } from "@material-ui/core";
 import LocationOnIcon from "@material-ui/icons/LocationOn";
 import PhoneIcon from "@material-ui/icons/Phone";
+import { Rating } from "@material-ui/lab";
 import React from "react";
 import useStyles from "./style";
 const PlaceDetails = ({ place }) => {
@@ -29,6 +30,10 @@ const PlaceDetails = ({ place }) => {
         <Typography gutterBottom variant="h5">
           {place.name}
         </Typography>
+        <Box display="flex" justifyContent="space-between" my={2}>
+          <Rating name="read-only" value={Number(place.rating)} readOnly />
+          <Typography component="legend">{place.num_reviews} review{place.num_reviews > 1 && 's'}</Typography>
+        </Box>
         <Box display="flex" justifyContent=" space-between">
           <Typography variant="subtitle1">Price</Typography>
           <Typography variant="subtitle1">{place.price_level}</Typography>
